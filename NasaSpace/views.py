d8a55aaeccbf6e2842eca139cc40031342761097
Nikhil Sardana <nikhilklk@gmail.com>
2016-04-23 10:03:00 +0530

    Initial commit

---
+++ NasaSpace/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render,render_to_response
+from django.http import HttpResponseRedirect,HttpResponse
+from django.template.context_processors import csrf
+from models import *
+from tweepy import Stream,OAuthHandler
+from tweepy.streaming import StreamListener
+import tweepy
+import os
+
+ckey = 'nwzgTPY9sYr0L70x61Z7Qzd5Z'
+csecret = 'dRrOCyqH1Jp2j6fv0O9F4yPhvYXF0VmMx8ieyav30E275O13zs'
+atoken = '63136441-BXcHjrKjvefkRbMYXho0ZFvShyGKLBbTUD03wizKB'
+asecret = 'Ht3j7ChKwN0lb21hCo6HRMoUFIiOSzKmv1sjggQFT98vF'
+
+class listener(StreamListener):
+    def on_data(self, data):
+        print(data)
+        return(True)
+
+    def on_error(self, status):
+        print status
+
+def testView2(request):
+    auth = OAuthHandler(ckey,csecret)
+    auth.set_access_token(atoken,asecret)
+    #twitterStream = Stream(auth,listener())
+    #print twitterStream.filter(track=["car"])
+    #text = twitterStream.filter(track=["car"])
+    api = tweepy.API(auth)
+    value = tweepy.Cursor(api.friends).items()
+    value = value.next()
+    print type(value)
+    #value='hello'
+    return HttpResponse(value)
+
+def uploadImage(request):
+    auth = OAuthHandler(ckey,csecret)
+    auth.set_access_token(atoken,asecret)
+    api = tweepy.API(auth)
+    status = "testing"
+    fn = os.path.abspath('/home/niks/Desktop/download.jpg')
+    api.update_with_media(fn,status)
+    return HttpResponse('Done')
+
+def authorizeAndShowFriends(request):
+    auth = OAuthHandler(ckey,csecret)
+    callback_url = 'http://127.0.0.1:8000/test'
+    auth = tweepy.OAuthHandler(ckey, csecret, callback_url)
+    if not request.GET.get('oauth_token'):
+        return HttpResponseRedirect(auth.get_authorization_url())
+    request.session['request_token'] = request.GET.get('oauth_token')
+    print "auth.request_token:",request.GET.get('oauth_token')
+
+    #auth.set_access_token(atoken,asecret)
+    auth.access_token = request.session.get('request_token')
+    api = tweepy.API(auth)
+    friends = api.friends()
+    print "Friends:",friends
+    return HttpResponse(friends)
+
+def index(request):
+    return render_to_response('index.html',{})
---
+++ OffToMoon/urls.py
@@ -0,0 +1,36 @@
+"""OffToMoon URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from NasaSpace import views
+
+urlpatterns = [
+    #actual urls
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index),
+
+    url(r'^whereAreYouMoon$', views.index),
+    url(r'^storyteller$', views.index),
+    url(r'^tripToApollo$', views.index),
+    url(r'^moonQuiz$', views.index),
+
+    #test urls
+    url(r'^uploadImage$', views.uploadImage), #upload image on twitter
+    url(r'^test$', views.authorizeAndShowFriends), #show twitter friends
+    url(r'^t$', views.testView2),
+    #url(r'^latestTweets$', views.latest_tweet),
+    #url(r'^callBackUrl/', views.callBackView),
+]
