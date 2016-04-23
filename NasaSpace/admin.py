a83f657507ce79b35396a9473ee3c3cda2ae030e
Nikhil Sardana <nikhilklk@gmail.com>
2016-04-23 14:26:15 +0530

    Added StoryAboutMoon model,misc changes

--- NasaSpace/admin.py
+++ NasaSpace/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from NasaSpace.models import *
 # Register your models here.
+
+admin.site.register(StoryAboutMoon)
--- NasaSpace/models.py
+++ NasaSpace/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+typeChoices = ( ('Myth','Myth'),('Fact','Fact'),('Story','Story'), )
+
 # Create your models here.
+class StoryAboutMoon(models.Model):
+    text = models.CharField(max_length = 1000,blank=True,null=True)
+    type = models.CharField(max_length = 10,choices = typeChoices,blank=True,null=True)
+    image = models.ImageField(blank=True,null=True)
